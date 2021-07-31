a76f570adebc60a4c9f5917903d4ae6731ee0288
baidyanathprasad <baidyanathprasadmdb@gmail.com>
2021-08-01 01:30:58 +0530

    Lint issues fixed

--- src/com/baidyanathprasad/jxrs/Application.kt
+++ src/com/baidyanathprasad/jxrs/Application.kt
@@ -3,7 +3,7 @@ package com.baidyanathprasad.jxrs
 import org.glassfish.jersey.server.ResourceConfig
 
 class Application : ResourceConfig() {
-    
+
     init {
         // Configure resource files for end points
         packages(this.javaClass.`package`?.name + ".resources")
